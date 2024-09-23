db920eca34c8e01f166895a5ab70e797df438985
Midnightgb <midnight3424@gmail.com>
2024-09-23 07:54:16 -0500

    Update main.py [ready for deploy]

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://senasoft.javm.tech", "http://localhost:5173"],
+    allow_origins=["https://puretech.javm.tech", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

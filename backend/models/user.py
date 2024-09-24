1ea38925fa82ec3f8086fbc1dbda0443410973a6
midnightgb <midnight3424@gmail.com>
2024-09-24 14:16:16 -0500

    Add database creation and table definition for users

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,12 +1,25 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, TIMESTAMP, DateTime, Enum
+from datetime import datetime
+from models.base_class import Base
+from enum import Enum as PyEnum
 
-Base = declarative_base()
+class Rol(PyEnum):
+    Admin = "Administrador"
+    Employee = "Employee"
+    vulnerable = "vulnerable"
+    non_vulnerable = "no_vulnerable"
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
+    name = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
+    rol = Column(Enum(Rol), nullable=False, default=Rol.no_vulnerable)
+    eco_points = Column(Integer, default=0)
+    created_at = Column(TIMESTAMP, default=datetime.now())
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    
+
+

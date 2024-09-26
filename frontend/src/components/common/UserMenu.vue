0fcb7b6ce354c70eaf75339b15153a3c71c0f0cb
Midnightgb <midnight3424@gmail.com>
2024-09-26 11:07:38 -0500

    Update UserMenu.vue

--- frontend/src/components/common/UserMenu.vue
+++ frontend/src/components/common/UserMenu.vue
@@ -46,7 +46,7 @@
       >
       <a
         href="#"
-        class="block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600"
+        class="hidden px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600"
         role="menuitem"
         >Cerrar sesión</a
       >

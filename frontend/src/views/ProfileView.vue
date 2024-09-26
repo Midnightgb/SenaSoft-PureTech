9fe9f7748d26d28dc7cb946c4d8c63aa08edd274
midnightgb <midnight3424@gmail.com>
2024-09-26 08:31:17 -0500

    Refactor profile image source URL in ProfileView.vue

--- frontend/src/views/ProfileView.vue
+++ frontend/src/views/ProfileView.vue
@@ -7,7 +7,7 @@
         class="bg-white dark:bg-gray-800 rounded-lg p-4 shadow mb-4 text-center transition-colors duration-200"
       >
         <img
-          src="/img/placeholder.svg"
+          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
           alt="Profile"
           class="w-24 h-24 rounded-full mx-auto mb-2"
         />
